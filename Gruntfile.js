61384b86f3210545620bfd610c1e1934b7c2fe37
mcekiera <marcin.cekiera@gmail.com>
2016-11-02 02:11:32 +0100

    dir structure

---
+++ Gruntfile.js
@@ -0,0 +1,52 @@
+module.exports = function(grunt) { 
+	// Project configuration. 
+	grunt.initConfig({ 
+		sass: { 
+			options: { 
+				sourceMap: true 
+			}, 
+				dist: { 
+				files: { 
+					'css/style.css': 'scss/style.scss' 
+				} 
+			} 
+		},
+		watch: { 
+			scripts: { 
+				files: ['scss/*.scss'], 
+				tasks: ['sass'], 
+				options: { 
+					spawn: false, 
+				}, 
+			} 
+		},
+		browserSync: {
+			default_options: {
+				bsFiles: {
+					src: [
+						"*js",
+						"js/*.js",
+						"css/*.css",
+						"ico/*.css",
+						"scss/*.scss",
+						"*.html"
+					]
+				},
+					options: {
+						watchTask: true,
+					server: {
+						baseDir: "./"
+					}
+				}
+			}
+		}
+
+	}); 
+	// Load the plugins tasks 
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-browser-sync');
+	// Default task(s). 
+
+	grunt.registerTask('default', ['browserSync', 'watch']); 
+};
---
+++ js/reveal.js
@@ -0,0 +1,4 @@
+window.sr = ScrollReveal();
+sr.reveal('p');
+sr.reveal('.service__icon', {duration: 1000}, 50);
+sr.reveal('.service__caption', {duration: 1000}, 50);
