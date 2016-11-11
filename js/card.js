00e21b41719b0d9b3d78c1c1a5c3c6a3635e94de
mcekiera <marcin.cekiera@gmail.com>
2016-11-11 20:31:04 +0100

    working original position card comeback

--- js/card.js
+++ js/card.js
@@ -2,6 +2,31 @@
  * Created by Pacin on 2016-11-11.
  */
 
+
 $(function() {
-	$( ".card" ).draggable();
+
+
+	var card = $( ".card" ).draggable({
+		zIndex: 99999999,
+		stack: ".card",
+
+		drag: function(){
+			var offset = $(this).offset();
+			var xPos = offset.left;
+			var yPos = offset.top;
+			console.log(xPos);
+			console.log(yPos);
+		},
+
+		revert : function() {
+			$(this).data("uiDraggable").originalPosition = {
+				top : 0,
+				left : 0
+			};
+			return true;
+		}
+	});
+	$(".card").droppable();
+
+
 });
