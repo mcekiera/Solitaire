4623c5ef3a9e774560c6f5adacd0eb974cf134b8
mcekiera <marcin.cekiera@gmail.com>
2016-11-11 01:58:43 +0100

    cards design: spades

--- js/board.js
+++ js/board.js
@@ -1,43 +1,43 @@
 function Board() {
-    "use strict";
+	"use strict";
 
-    var that = this;
+	var that = this;
 
-    this.fundations = {
-        tiles: [],
-        hearts: [],
-        clubs: [],
-        spades: []
-    };
+	this.fundations = {
+		tiles: [],
+		hearts: [],
+		clubs: [],
+		spades: []
+	};
 
-    this.tableu = {
-        "1" : [],
-        "2" : [],
-        "3" : [],
-        "4" : [],
-        "5" : [],
-        "6" : [],
-        "7" : []
-    };
+	this.tableu = {
+		"1": [],
+		"2": [],
+		"3": [],
+		"4": [],
+		"5": [],
+		"6": [],
+		"7": []
+	};
 
-    this.stock = [];
-    this.talon = [];
+	this.stock = [];
+	this.talon = [];
 
-    this.set = function (deck) {
-            for (var piles = 1; piles <= 7; piles++) {
-                    if (that.tableu.hasOwnProperty(piles)) {
-                            for (var i = 0; i < piles; i++) {
-                                var card = deck.getCard();
-                                that.tableu[piles].push(card);
-                            }
+	this.set = function (deck) {
+		for (var piles = 1; piles <= 7; piles++) {
+			if (that.tableu.hasOwnProperty(piles)) {
+				for (var i = 0; i < piles; i++) {
+					var card = deck.getCard();
+					that.tableu[piles].push(card);
+				}
 
-                    }
-            }
+			}
+		}
 
-            while (deck.cards.length > 0) {
-                that.stock.push(deck.getCard());
-            }
-    };
+		while (deck.cards.length > 0) {
+			that.stock.push(deck.getCard());
+		}
+	};
 }
 
 
--- js/deck.js
+++ js/deck.js
@@ -1,31 +1,31 @@
 function Deck() {
-    "use strict";
-    
-    var that = this;
-    var colors = ["Spades", "Clubs", "Hearts", "Tiles"];
-    var figures = [1,2,3,4,5,6,7,8,9,10,11,12,13];
-    var set = [];
+	"use strict";
 
-    for (var i = 0; i < colors.length; i++) {
-        for (var j = 0; j < figures.length; j++) {
-            set.push(new Card(figures[j],colors[i]));
-        }
-    }
+	var that = this;
+	var colors = ["Spades", "Clubs", "Hearts", "Tiles"];
+	var figures = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+	var set = [];
 
-    this.cards =  set;
-    this.getCard = function() {
-        var rand = Math.random() * that.cards.length;
-        return that.cards.pop();
-    };
+	for (var i = 0; i < colors.length; i++) {
+		for (var j = 0; j < figures.length; j++) {
+			set.push(new Card(figures[j], colors[i]));
+		}
+	}
+
+	this.cards = set;
+	this.getCard = function () {
+		var rand = Math.random() * that.cards.length;
+		return that.cards.pop();
+	};
 }
 
 function Card(figure, color) {
-    "use strict";
+	"use strict";
 
-    var that = this;
-    this.figure = figure;
-    this.color = color;
-    this.toString = function() {
-        return that.col + that.fig;
-    };
+	var that = this;
+	this.figure = figure;
+	this.color = color;
+	this.toString = function () {
+		return that.col + that.fig;
+	};
 }
