536d8cd8cb4e16931f7ab124182949b8574bee86
mcekiera <marcin.cekiera@gmail.com>
2016-11-18 23:25:49 +0100

    working reveling cards from stack on click function

--- js/solitaire.js
+++ js/solitaire.js
@@ -4,7 +4,16 @@ var solitaire = function() {
 	var deck = new Deck();
 	var board = new Board(deck);
 
+	$('.uncovered').draggable({
+		zIndex: 99999999,
+		stack: ".card"
+	});
 
+	$('#js-stack').click(function () {
+		var card = board.stack.cards.pop();
+		card.$element.addClass('uncovered');
+		board.waste.addCard(card);
+	})
 
 
 
